56fca0881a6f50e88637d381c7f252f709c670d9
JRBarreraM <mrjrbarreram@gmail.com>
2019-09-20 21:09:12 -0400

    Fix not attach conflicts

--- Laboratorio0/script.R
+++ Laboratorio0/script.R
@@ -1,15 +1,15 @@
 #Estudiantes:
-# Jose Barrera 15-10123
+# Jose Barrera  15-10123
 # Carlos Sivira 15-11377
 ####################################################################
 #Pregunta 1
 poll <- read.table("CO3321_lab0.txt", header = TRUE)
 ####################################################################
 #Pregunta 2
-length(poll$Sexo[Preg1 =="SI" & Sexo == "F"])
+length(poll$Sexo[poll$Preg1 =="SI" & poll$Sexo == "F"])
 ####################################################################
 #Pregunta 3
-length(poll$Edad[Edad > 40])
+length(poll$Edad[poll$Edad > 40])
 ####################################################################
 #Pregunta 4
 mean(subset(poll, (Preg3 == "SI" & Preg5 == 1),select=Preg4)$Preg4)
