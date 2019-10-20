ec2c9951435de2398bc312d36944da2db555ca2e
Carlos Sivira <carlos.csivira@gmail.com>
2019-10-20 11:03:24 -0400

    Parte2

--- Laboratorio2/script_Lab2_1510123_1511377.R
+++ Laboratorio2/script_Lab2_1510123_1511377.R
@@ -12,11 +12,32 @@ data1 <- c(6.9, 7.6, 6.5, 6.2, 5.3, 7.8, 7.0, 5.5, 7.6, 6.7, 7.3, 6.6, 7.1, 6.9,
 
 ####################################################################
 #Pregunta 2
-
+#Datos del problema 2
 data2 <- c(15, 16, 14, 15, 17, 18, 19, 15, 13, 12, 11, 13, 11, 9, 10, 10)
-
+#Definicion de la matriz y sus respectivos headers por columna
 dataMatrix <- matrix(data2, nrow=8)
 colnames(dataMatrix) <- c("Método I", "Método II")
-dataMatrix
 
+#------------------------------Seccion 1------------------------------
+#Obtencion del summary
+summary(dataMatrix)
+#Calculo del coeficiente de variacion para cada columna
+sd(dataMatrix[, 1]) / mean(dataMatrix[,1])
+sd(dataMatrix[, 2]) / mean(dataMatrix[,2])
+#Muestra de las graficas
+#par(mfrow=c(3,2))
+hist(dataMatrix[,1], ylab = 'Cantidad de ciclistas', main='Tiempos por ciclisas Método I', col = 'green')
+#hist(dataMatrix[,2], ylab = 'Cantidad de ciclistas', main='Tiempos por ciclisas Método II', col = 'green')
+#qqnorm(dataMatrix[,1])
+#qqline(dataMatrix[,1])#Metodo 1
+#qqnorm(dataMatrix[,2])
+#qqline(dataMatrix[,2])#Metodo 2
+#boxplot(dataMatrix, ylab = 'Tiempos', main='Tiempos por ciclisas en ambos métodos', col = 'green')
+
+#------------------------------Seccion 2------------------------------
+var.test(dataMatrix[,1], dataMatrix[,2])$conf.int
+t.test (dataMatrix[,1], dataMatrix[,2], var.equal = T, conf.level = 0.95 )$conf.int
+
+#------------------------------Seccion 3------------------------------
+var(dataMatrix[,1]) - var(dataMatrix[,2])
 ####################################################################
