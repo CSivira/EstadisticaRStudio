bab5f1aeebad688030aba0b2613444cd9c2e8f3c
Carlos Sivira <carlos.csivira@gmail.com>
2019-10-20 09:21:01 -0400

    Matrices

--- Laboratorio2/script_Lab1_1510123_1511377.R
+++ Laboratorio2/script_Lab1_1510123_1511377.R
@@ -86,3 +86,26 @@ posAns5 = subset(poll, (Preg3 == 'SI'), select=Preg5)$Preg5
 #        ylab = 'Gasto en buhoneria', 
 #        main='Gasto en buhoneria según línea de metro', 
 #        col = 'blue')
+
+
+#Laboratorio 2. Intervalos de Confianza
+#Estudiantes:
+# José Barrera  15-10123
+# Carlos Sivira 15-11377
+
+####################################################################
+#Pregunta 1
+
+####################################################################
+#Pregunta 2
+
+####################################################################
+
+data1 <- c(6.9, 7.6, 6.5, 6.2, 5.3, 7.8, 7.0, 5.5, 7.6, 6.7, 7.3, 6.6, 7.1, 6.9, 6.0, 6.8, 6.5, 7.2, 5.8, 8.6, 7.6, 7.1, 6.0, 7.2, 7.7)
+
+data2 <- c(15, 16, 14, 15, 17, 18, 19, 15, 13, 12, 11, 13, 11, 9, 10, 10)
+
+dataMatrix <- matrix(data2, nrow=8)
+colnames(dataMatrix) <- c("Método I", "Método II")
+
+dataMatrix
