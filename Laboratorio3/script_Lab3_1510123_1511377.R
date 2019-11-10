953bcd8c00ced6032a165637d4c97e46b93e4840
JRBarreraM <mrjrbarreram@gmail.com>
2019-11-09 20:20:52 -0400

    Script lab 3

---
+++ Laboratorio3/script_Lab3_1510123_1511377.R
@@ -0,0 +1,8 @@
+#Laboratorio 3. Pruebas de hipótesis y pruebas Chi-Cuadrado
+#Estudiantes:
+# José Barrera  15-10123
+# Carlos Sivira 15-11377
+
+####################################################################
+#Pregunta 1
+####################################################################
