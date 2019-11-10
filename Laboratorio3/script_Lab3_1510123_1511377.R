a9fae7e243bd917c698790913060f6be10eb4a76
Carlos Sivira <carlos.csivira@gmail.com>
2019-11-09 23:21:23 -0400

    Final Lab3

--- Laboratorio3/script_Lab3_1510123_1511377.R
+++ Laboratorio3/script_Lab3_1510123_1511377.R
@@ -1,6 +1,6 @@
-#Laboratorio 3. Pruebas de hipótesis y pruebas Chi-Cuadrado
+#Laboratorio 3. Pruebas de hipotesis y pruebas Chi-Cuadrado
 #Estudiantes:
-# José Barrera  15-10123
+# Jose Barrera  15-10123
 # Carlos Sivira 15-11377
 
 ####################################################################
@@ -12,7 +12,7 @@ Nuevo  <- c(276, 222, 310, 338, 200, 302, 317, 260, 320, 312, 334, 265)
 #Se verifica si las varianzas pueden ser iguales
 var.test(Actual, Nuevo)$conf.int
 #------------------------------Seccion 2------------------------------
-#Se c�lcula la prueba de hipotesis
+#Se calcula la prueba de hipotesis
 t.test (Actual, Nuevo, var.equal = TRUE, conf.level = 0.93, alternative = "greater")
 ####################################################################
 #Pregunta 2
@@ -25,22 +25,23 @@ k <- length(fi)
 lambda <-  2.435
 #No se pierden grados de libertad, por lo tanto r = 0
 r <- 0
-#Calculo del estadistico chi cuadrado
+#Calculo del estadistico chi-cuadrado
 pi <- (((lambda^mi)*(exp(-lambda))) / factorial(mi))
 pi[k] = 1 - (pi[1] + pi[2] + pi[3] + pi[4] + pi[5] + pi[6])
 en <- (n * pi)
 chi_square <- sum((fi - en)^2 / en)
-#Se obtiene el valor de chi cuadrado para el alpha dado
+#Se obtiene el valor de chi-cuadrado para el alpha dado
 chi_value <- qchisq(1 - alpha, k - 1 - r)
 #Se obtiene el p-valor asociado
 p_value <- pchisq(chi_square, k - 1 - r)
 ####################################################################
 #Pregunta 3
+#Datos del problema
 Oscura <- c(31, 26, 18)
 Clara  <- c(19, 40, 31)
 Ligera <- c(10, 15, 22)
-
+#Creando la matriz del problema
 matriz <- cbind(Oscura, Clara, Ligera)
-
+#Calculo del p-valor asociado a los datoa dados
 chisq.test(matriz)
 ####################################################################
